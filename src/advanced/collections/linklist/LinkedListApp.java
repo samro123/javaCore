18a7d79291c85a5b1ee3235f043ec52ca54e08ce
samro123 <thsam.20it5@vku.udn.vn>
2024-07-23 09:54:24 +0700

    linklist

---
+++ src/advanced/collections/linklist/LinkedListApp.java
@@ -0,0 +1,33 @@
+package advanced.collections.linklist;
+
+import java.util.LinkedList;
+
+public class LinkedListApp {
+    public static void main(String[] args) {
+        LinkedList<String> morningRoutine = new LinkedList<>();
+
+
+        if(!morningRoutine.isEmpty()){
+            System.out.println("The first element of the list: " + morningRoutine.getFirst());
+        }
+        if(!morningRoutine.isEmpty() && morningRoutine.peekFirst() != null){
+            System.out.println("The first element of the list: " + morningRoutine.peekFirst().toUpperCase());
+        }
+
+//        morningRoutine.add("Brushing my teeth");
+//        morningRoutine.add("Taking the dog for walk");
+//        morningRoutine.add("Doing 32 push-ups");
+
+        morningRoutine.add("Brushing my teeth");
+        morningRoutine.addFirst("Taking the dog for walk");
+        morningRoutine.push("Doing 32 push-ups");
+        morningRoutine.offer("Reading 1 chapter of a book");
+
+        System.out.println("I used pop function: "+ morningRoutine.pop());
+        System.out.println(morningRoutine);
+
+        System.out.println("I completed the first element on my list: " + morningRoutine.pollFirst());
+        System.out.println("I completed the first element on my list: " + morningRoutine.pollLast());
+        System.out.println(morningRoutine);
+    }
+}
